ad94e9a585b8b3c2f65503f6d956201db6445801
Luis Hernández <ldhdv95@gmail.com>
2018-08-21 14:27:15 -0500

    Add buttons to start and stop the game, also function for: grow carrots, seed weed and kill weed

--- js/app.js
+++ js/app.js
@@ -4,7 +4,25 @@ const plants = document.getElementById('plants');
 const garden = document.getElementById('garden');
 const water = document.getElementById('water');
 const poison = document.getElementById('poison');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
 let selection;
+
+// Objeto para los intervalos de tiempo para cada elemento
+let intervals = {
+  seedWeed: null,
+  killWeed: null,
+  growCarrots: null
+};
+
+let started = false;
+const max = 25;
+
+// Estado para la persistencia de datos
+const state = {
+  plants: [],
+  garden: []
+};
 
 // Crear elementos a plantar
 function createGardenElement(type) {
@@ -15,13 +33,62 @@ function createGardenElement(type) {
   return element;
 }
 
+function seedWeed() {
+  // Posicion aleatoria 0-25 en la grilla
+  let position = Math.floor(Math.random() * max);
+  // Verifico si la posicion no esta ocupada por alguna planta o agua para colocar poison
+  if (!state.plants[position] && state.garden[position] !== 'water') {
+    let element = createGardenElement('weed');
+    plants.children[position].innerHTML = '';
+    plants.children[position].appendChild(element);
+    state.plants[position] = 'weed';
+  }
+}
+
+function killWeed() {
+  /* Recorro el estado de plantas y verifico si hay weed  y en esa misma posicion
+   en el garden hay poison para eliminar ambos
+   */
+  state.plants.forEach((plant, index) => {
+    if (plant === 'weed' && state.garden[index] === 'poison') {
+      state.plants[index] = undefined;
+      state.garden[index] = undefined;
+      plants.children[index].innerHTML = '';
+      garden.children[index].innerHTML = '';
+    }
+  });
+}
+
+function growCarrots() {
+  /* Recorro el estado de garden y verifico si hay agua en esa posicion para 
+   colocar una zanahoria */
+  state.garden.forEach((resource, index) => {
+    if (resource === 'water') {
+      state.garden[index] = undefined;
+      garden.children[index].innerHTML = '';
+      let element = createGardenElement('carrot');
+      plants.children[index].innerHTML = '';
+      plants.children[index].appendChild(element);
+      state.plants[index] = 'carrot';
+    }
+  });
+}
+
 // Colocare el elemento seleccionado(Water o Poison) en la grilla(overlay)
 overlay.addEventListener('click', (event) => {
-  if (selection) {
+  if (started && selection) {
     let position = parseInt(event.target.getAttribute('id'));
-    let element = createGardenElement(selection); 
-    garden.children[position].innerHTML = '';
-    garden.children[position].appendChild(element);
+    if (!state.garden[position] &&
+      (
+        (selection === 'poison' && state.plants[position] === 'weed') ||
+        (selection === 'water' && state.plants[position])
+      )
+    ){
+      let element = createGardenElement(selection);
+      garden.children[position].innerHTML = '';
+      garden.children[position].appendChild(element);
+      state.garden[position] = selection;
+    }
   }
 });
 
@@ -36,4 +103,21 @@ poison.addEventListener('click', (event) => {
   selection = 'poison';
   poison.classList.add('selected');
   water.classList.remove('selected');
+});
+
+// EVENTO INTERVALOS DE TIEMPO PARA CADA UNO DE LOS ELEMENTOS JUEGO EN MODO PLAY
+play.addEventListener('click', (event) => {
+  intervals.growCarrots = setInterval(growCarrots, 4000);
+  intervals.seedWeed = setInterval(seedWeed, 1500);
+  intervals.killWeed = setInterval(killWeed, 500);
+  play.classList.add('selected');
+  started = true;
+});
+ //  EVENTO INTERVALOS DE TIEMPO PARA CADA UNO DE LOS ELEMENTOS JUEGO EN MODO STOP
+stop.addEventListener('click', (event) => {
+  clearInterval(intervals.growCarrots);
+  clearInterval(intervals.seedWeed);
+  clearInterval(intervals.killWeed);
+  play.classList.remove('selected');
+  started = false;
 });
